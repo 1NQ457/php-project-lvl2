1908f130d6790a9dbc3d9792354b34b16432c272
1NQ457 <alex.kot14@gmail.com>
2021-02-12 00:27:00 +0300

    fixes

--- src/Formatters/Json.php
+++ src/Formatters/Json.php
@@ -4,5 +4,5 @@ namespace Differ\Formatters\Json;
 
 function json($tree): string
 {
-    return json_encode($tree, JSON_PRETTY_PRINT) ? json_encode($tree, JSON_PRETTY_PRINT) : '';
+    return (string) json_encode($tree, JSON_PRETTY_PRINT);
 }
